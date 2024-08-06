36cdac54ac00f3f4dfb3baa08fe9c7d9b5779908
parmar-viral <parmarviral8324@gmail.com>
2024-08-06 06:50:07 +0530

    files add

---
+++ admin/controller/database/create_products_tables.php
@@ -0,0 +1,48 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "Agro";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $database);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+// sql to create product table
+$sql="CREATE TABLE products (
+    product_id INT AUTO_INCREMENT PRIMARY KEY,
+    product_name VARCHAR(100) NOT NULL,
+    description TEXT,
+    product_price DECIMAL(10, 2) NOT NULL,
+    product_image VARCHAR(255),
+    category VARCHAR(50),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+)";
+if(mysqli_query($conn, $sql)) {
+    echo "Table products created successfully";
+} else {
+    echo "Error creating table: " . mysqli_error($conn);
+}
+
+
+//sql to create feedback table
+$sql = "CREATE TABLE feedback (
+        name VARCHAR(255)NOT NULL,
+        email VARCHAR(255)NOT NULL,
+        message VARCHAR(255)NOT NULL
+        
+    )";
+if (mysqli_query($conn, $sql)) {
+    echo "Table created successfully";
+} else {
+    echo "Error creating table: " . mysqli_error($conn);
+}
+
+
+mysqli_close($conn);
+
+?>
